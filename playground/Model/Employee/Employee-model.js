d377856d685a689642e9d0e540449d63a7fc5af7
Stefan Eugster <stefan.eugster@llv.li>
2014-12-01 08:04:34 +0100

    misc bugfixes JQDateTime

--- playground/Model/Employee/Employee-model.js
+++ playground/Model/Employee/Employee-model.js
@@ -9,8 +9,14 @@ model.Employee.lastName = new Attribute('storage', 'string');
 model.Employee.firstName = new Attribute('storage', 'string');
 model.Employee.comment = new Attribute('storage', 'string');
 model.Employee.dateOfBirth = new Attribute('storage', 'date', {
-	simpleDate: false
+	simpleDate: true
 });
 model.Employee.email = new Attribute('storage', 'string');
 model.Employee.phone = new Attribute('storage', 'string');
 model.Employee.age = new Attribute('calculated', 'number');
+model.Employee.createDate = new Attribute('storage', 'date', {
+	simpleDate: false
+});
+model.Employee.modifiedDate = new Attribute('storage', 'date', {
+	simpleDate: false
+});
