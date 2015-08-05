5fa0490300e2745e1dd81f66c1bf6e3efd62c81e
Stefan Eugster <stefan.eugster@llv.li>
2015-08-05 07:33:36 +0200

    add request handler to bootstrap

--- playground/bootstrap.js
+++ playground/bootstrap.js
@@ -1,2 +1,16 @@
 ﻿
+/**
+ * http request handler
+ */
+
+// redirect to https handler
+//addHttpRequestHandler('^/', 'HttpRequestHandler/redirect.js', 'redirectToHttps');
+// download handler
+addHttpRequestHandler('^/download/', 'HttpRequestHandler/download.js', 'download');
+
+
+/**
+ * remove temporary content at startup
+ */
+ 
 Folder(ds.getDataFolder().path + 'tmp/').removeContent();
