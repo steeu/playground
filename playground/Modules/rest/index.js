66386ea1d19cd708b5ef4ef66833446b639428e1
Stefan Eugster <stefan.eugster@llv.li>
2015-08-06 09:16:44 +0200

    rest module

---
+++ playground/Modules/rest/index.js
@@ -0,0 +1,88 @@
+
+// load depending modules
+var curlModule = require('curl');
+
+/**
+ * send email messages
+ *
+ * @return {Object} result
+ */
+ 
+var REST = function(options) {
+	try {
+	    var options = options || {};
+	    
+	    // settings
+	    this.proxy    = options.proxy || null;
+	    this.port     = options.port || 8080;
+	    this.user     = options.user || null;
+	    this.password = options.password || null;
+	} catch (e) {
+		WAKTOOLS.log(e);
+		return e;
+	}
+}
+
+
+/**
+ * connect to mailserver
+ *
+ * @return {Object} result
+ */
+
+REST.prototype.JSON = function(url, data) {
+	try {
+        var curl,
+        	command = url,
+        	result = {success: 0, error: '', result: null},
+        	resultStr,
+        	resultJSON;
+		
+		// add data
+		if (data) {
+			command = '--data ' + data + ' ' + command;
+		}
+		// add user
+		if (this.user) {
+			command = '--proxy-user ' + this.user + ':' + this.password + ' ' + command;
+		}
+		// add proxy
+		if (this.proxy) {
+			command = '--proxy ' + this.proxy + ':' + this.port + ' ' + command;
+		}
+		// command
+		result.command = 'curl ' + command;
+		// execute
+        curl = curlModule.curl(command);
+        // validate result
+        if (curl.console && curl.console.stdIn && curl.console.stdOut) {
+        	// validate stdOut
+        	if (curl.console.stdOut.length > 0) {
+        		resultStr = curl.console.stdOut.toString('utf-8');
+        	}
+        	// parse json string
+	        try {
+				// parse result
+				result.result = JSON.parse(resultStr);
+				result.success = 1;	
+			} catch (e) {
+				result.error = e.message;
+				result.result = curl.console.stdOut;
+				
+				return result;
+			}
+        } else if (curl.console && curl.console.stdErr) {
+        	result.error = 'curl execution failed';
+        	result.result = curl;
+        	
+        	return result;
+        }
+
+		return result;
+	} catch (e) {
+		WAKTOOLS.log(e);
+		return e;		
+	}
+};
+
+exports.REST = REST;
